6e33dfc288f029701ea1bff0a8d09df7215eaeef
lfeq <panchocosa@gmail.com>
2024-07-12 19:24:36 -0600

    Finished video 6

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,19 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
     [SerializeField] private float movementSpeed = 3.5f;
+    [SerializeField] private float rotateSpeed = 100f;
 
     private Rigidbody m_rb;
-    
+
     // Start is called before the first frame update
     void Start() {
         m_rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update() {
+    void FixedUpdate() {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+        Vector3 rotateY = new Vector3(0, Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime, 0);
+        if (movement != Vector3.zero) {
+            m_rb.MoveRotation(m_rb.rotation *  Quaternion.Euler(rotateY));
+        }
         m_rb.MovePosition(m_rb.position +
                           transform.forward * (Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime) +
                           transform.right * (Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime));
