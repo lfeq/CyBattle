2c31830c47534133b1edd614880a72e0a98afeff
lfeq <panchocosa@gmail.com>
2024-07-29 16:16:05 -0600

    Finished video 46

--- Assets/Scripts/UI/PlayerCheck.cs
+++ Assets/Scripts/UI/PlayerCheck.cs
@@ -1,3 +1,4 @@
+using System;
 using Photon.Pun;
 using TMPro;
 using UnityEngine;
@@ -5,12 +6,23 @@ using UnityEngine;
 public class PlayerCheck : MonoBehaviour {
     [SerializeField] private int maxPlayersInRoom = 2;
     [SerializeField] private TMP_Text currentPlayersText;
+    [SerializeField] private GameObject hint1, hint2, enterButton;
+
+    private void Start() {
+        enterButton.SetActive(false);
+    }
 
     void Update() {
         if (PhotonNetwork.CurrentRoom.PlayerCount == maxPlayersInRoom) {
             PhotonNetwork.CurrentRoom.IsOpen = false;
-            gameObject.SetActive(false);
+            hint1.SetActive(false);
+            hint2.SetActive(false);
+            enterButton.SetActive(true);
         }
         currentPlayersText.text = $"{PhotonNetwork.CurrentRoom.PlayerCount}/{maxPlayersInRoom} joined";
+    }
+
+    public void enterArena() {
+        gameObject.SetActive(false);
     }
 }
