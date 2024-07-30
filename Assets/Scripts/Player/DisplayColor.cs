9fe0b7d28c1f05cbb86b2ffa0b5df67b37ff8e26
lfeq <panchocosa@gmail.com>
2024-07-30 16:12:16 -0600

    Finished video 47, 48, 49

--- Assets/Scripts/Player/DisplayColor.cs
+++ Assets/Scripts/Player/DisplayColor.cs
@@ -1,17 +1,29 @@
 using System;
+using System.Collections;
 using Photon.Pun;
 using UnityEngine;
 
-public class DisplayColor : MonoBehaviour {
+public class DisplayColor : MonoBehaviourPunCallbacks {
     public int[] viewId;
 
     [SerializeField] private int[] buttonNumbers;
     [SerializeField] private Color[] colors;
     
     private GameObject m_namesObject;
+    private GameObject m_waitFoPlayers;
 
     private void Start() {
         m_namesObject = GameObject.Find("Names Background");
+        m_waitFoPlayers = GameObject.Find("Waiting Background");
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (GetComponent<PhotonView>().IsMine && !m_waitFoPlayers.activeInHierarchy) {
+                removeData();
+                roomExit();
+            }
+        }
     }
 
     public void chooseColor() {
@@ -27,5 +39,31 @@ public class DisplayColor : MonoBehaviour {
                 m_namesObject.GetComponent<NicknamesScript>().names[i].text = GetComponent<PhotonView>().Owner.NickName;
             }
         }
+    }
+
+    void removeData() {
+        GetComponent<PhotonView>().RPC("removeMe", RpcTarget.AllBuffered);
+    }
+
+    void roomExit() {
+        StartCoroutine(getReadyToLeave());
+    }
+
+    [PunRPC]
+    void removeMe() {
+        for (int i = 0; i < m_namesObject.gameObject.GetComponent<NicknamesScript>().names.Length; i++) {
+            if (GetComponent<PhotonView>().Owner.NickName ==
+                m_namesObject.GetComponent<NicknamesScript>().names[i].text) {
+                m_namesObject.GetComponent<NicknamesScript>().names[i].gameObject.SetActive(false);
+                m_namesObject.GetComponent<NicknamesScript>().healthBars[i].gameObject.SetActive(false);
+            }
+        }
+    }
+
+    IEnumerator getReadyToLeave() {
+        yield return new WaitForSeconds(1);
+        m_namesObject.GetComponent<NicknamesScript>().leaving();
+        Cursor.visible = true;
+        PhotonNetwork.LeaveRoom();
     }
 }
--- Assets/Scripts/UI/NicknamesScript.cs
+++ Assets/Scripts/UI/NicknamesScript.cs
@@ -1,16 +1,47 @@
-using System;
+using System.Collections;
+using Photon.Pun;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class NicknamesScript : MonoBehaviour {
+public class NicknamesScript : MonoBehaviourPunCallbacks {
     public TMP_Text[] names;
-    
-    [SerializeField] private Image[] healthBars;
+    public Image[] healthBars;
+
+    private GameObject waitObject;
 
     private void Start() {
+        waitObject = GameObject.Find("Waiting Background");
+        waitObject = GameObject.Find("Waiting Background");
         foreach (TMP_Text nameText in names) {
             nameText.gameObject.SetActive(false);
         }
+    }
+
+    public void returnToLobby() {
+        waitObject.SetActive(false);
+        roomExit();
+    }
+
+    public void leaving() {
+        StartCoroutine(nameof(backToLobby));
+    }
+
+    private void roomExit() {
+        StartCoroutine(toLobby());
+    }
+    
+    IEnumerator backToLobby() {
+        yield return new WaitForSeconds(0.5f);
+        PhotonNetwork.LoadLevel("Lobby");
+    }
+
+    IEnumerator toLobby() {
+        yield return new WaitForSeconds(0.1f);
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public override void OnLeftRoom() {
+        PhotonNetwork.LoadLevel("Lobby");
     }
 }
